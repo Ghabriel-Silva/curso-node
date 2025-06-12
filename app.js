3588c86d7826dc2f410d79402a412d82a0f24fc5
Gabriel <ghaabriieel@gmail.com>
2025-06-12 11:37:57 -0300

    Aprendendo a manipular o sql

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-
+/*
+req = tudo que vem do cliente para o servidor.
+res = tudo que você envia do servidor para o cliente.
+*/
 const express = require("express") //Importação do Express:
 
-const app = express();
+const app = express(); 
 
-app.get("/", function (req, res) { // sempre que uso apenas o / se refere a minha rota 
-    res.send('Pagina incial')
+app.get("/", function (req, res) { // sempre que uso apenas o barra se refere a minha rota 
+    res.sendFile(__dirname + "/html/index.html") //__dirname retorna o diretorio absoluto da aplicação
 })
 
-app.get('/produtos', function (req, res) { // estou criando uma rota simples
-    res.send('pagina de produtos')
+app.get('/sobre', function (req, res) { // estou criando uma rota simples
+    res.sendFile(__dirname + "/html/sobre.html")
 })
 
-app.get("/entregas/:id", function (req, res) { //Estou cliando uma rota e passando um parametro id, umm request
+app.get("/entregas/:id", function (req, res) { //Estou cliando uma rota e passando um parametro id, um request
     if (req.params.id == 1) {
-        res.send('entrega maria')
+        res.send('entrega maria'+ req.params.id)
     } else if (req.params.id !== 1) {
         res.send('Page is no found')
     }
