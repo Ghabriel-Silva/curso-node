ba1fd07d028db7b32c5d76b455e791c336c613d5
Gabriel <ghaabriieel@gmail.com>
2025-06-13 11:38:28 -0300

    Trabalhano com consultas de dados e adcionando dados no banco de dados

--- teste.sql
+++ teste.sql
@@ -1,5 +1,41 @@
+/*Criando uma tabela usuarios dentro do banco de dados*/
 CREATE TABLE usuarios(
     nome VARCHAR(50),
     email VARCHAR(100), 
     idade INT
 );
+
+ALTER TABLE usuarios ADD id INT; /*Adicionando uma nova coluna*/
+
+/*Inserindo dados dentro da minha tabela*/
+INSERT INTO  usuarios (nome, email, idade) VALUES(
+    "Gabriel silva", 
+    "ghaabriel@gmail.com",
+    23
+);
+
+/*Inserindo dados no ID*/
+
+
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Ketlin Ribeiro", 
+    "ketlin@gmail.com", 
+    21
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Rafael Silva", 
+    "rafan@gmail.com", 
+    23
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Josiane Aparecida", 
+    "josi@gmail.com", 
+    45
+);
+
+/*Editnado dados existente em uma Tabela*/
+UPDATE usuarios SET nome = "Gabriel" where id = 1;
+
