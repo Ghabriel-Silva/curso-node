9ae52f0b303cc64be11608583cacd6241d35e6ce
Gabriel <ghaabriieel@gmail.com>
2025-06-14 11:23:17 -0300

    Aprendendo mais sobre o mysql, aprendendo oque é um orm , trabalahando com sequelizer para cria e adicionar linha, aprendendo sor templete engine, e sobre handlears

--- app.js
+++ app.js
@@ -3,26 +3,10 @@ req = tudo que vem do cliente para o servidor.
 res = tudo que você envia do servidor para o cliente.
 */
 const express = require("express") //Importação do Express:
-
 const sequelize = require('./models/db')
-
 const app = express(); 
+const handlebars = require('express-handlebars')
 
-app.get("/", function (req, res) { // sempre que uso apenas o barra se refere a minha rota 
-    res.sendFile(__dirname + "/html/index.html") //__dirname retorna o diretorio absoluto da aplicação
-})
-
-app.get('/sobre', function (req, res) { // estou criando uma rota simples
-    res.sendFile(__dirname + "/html/sobre.html")
-})
-
-app.get("/entregas/:id", function (req, res) { //Estou cliando uma rota e passando um parametro id, um request
-    if (req.params.id == 1) {
-        res.send('entrega maria'+ req.params.id)
-    } else if (req.params.id !== 1) {
-        res.send('Page is no found')
-    }
-})
 
 app.listen(8081, function () {
     console.log('servior rodando...')
--- models/db.js
+++ models/db.js
@@ -1,19 +1,70 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
-    'cadastros', 
+    'cadastros',
     'root', //user banco de dados
-    '@Gs189970', 
+    '@Gs189970',
     {
-        host: "localhost", 
+        host: "localhost",
         dialect: 'mysql'
     }
 )
 sequelize.authenticate()
-.then(()=>{
-    console.log('Conexão com o banco de dados bem-sucedida.')
+    .then(() => {
+        console.log('Conexão com o banco de dados bem-sucedida.')
+    })
+    .catch((error) => {
+        console.log('Erro ao conectar com o banco de dados:', error)
+    })
+
+//Criando um tabela no banco de dados
+const Postagem = sequelize.define('postagem', {
+    titulo: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    conteudo: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
 })
-.catch((error)=>{
-    console.log('Erro ao conectar com o banco de dados:', error)
+
+//Criando tabelas
+/*Postagem.create({
+        titulo: 'Testando banco de dados!',
+        conteudo: 'e quiser, posso te ajudar a criar validadores personalizados'
+    })
+*/
+
+
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false, //campo n pode ser null
+        validate: {
+            max: 20
+        }
+
+    },
+    sobrenome: {
+        type: DataTypes.STRING
+    },
+    idade: {
+        type: DataTypes.INTEGER,
+        validate: {
+            isInt: true
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            isEmail: true,
+        }
+    }
 })
+
+
+
+//Usuario.sync({force: true})
 
 module.exports = sequelize
